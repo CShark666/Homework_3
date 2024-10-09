b3695af1b08045286e9e5829fa01fb11de5dd894
CShark666 <purplesloth.riario1337@gmail.com>
2024-10-09 23:21:36 +0300

    1.2

---
+++ Task_1/MyArray.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Task_1
+{
+    internal class MyArray : IOutput, IMath, ISort
+    {
+        private int[] _myArray;
+
+        public MyArray(int[] array)
+        {
+            this._myArray = array; 
+        }
+        public void Show()
+        {
+            Console.WriteLine("Array elements: ");
+            foreach (var item in _myArray)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        public void Show(string info)
+        {
+            Console.WriteLine(info);
+            Show();
+        }
+
+        public int Max()
+        {
+            return _myArray.Max();
+        }
+        public int Min()
+        {
+            return _myArray.Min();
+        }
+
+        public float Avg()
+        {
+            return (float)_myArray.Average();
+        }
+
+        public bool Search(int value)
+        {
+            foreach (var item in _myArray)
+            {
+                if (item == value)
+                    return true;
+            }
+            return false;
+        }
+
+        public void SortAsc()
+        {
+            Array.Sort(_myArray);
+        }
+        public void SortDesc()
+        {
+            Array.Sort(_myArray);
+            Array.Reverse(_myArray);
+        }
+        public void SortByParam(bool parameter)
+        {
+            if (parameter)
+            {
+                SortAsc();
+            }
+            else
+            {
+                SortDesc();
+            }
+
+        }
+    }
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Task_1;
+
+//Task 1
+
+int[] nums = { 1, 2, 3, 4, 5, };
+var TestMyArray = new MyArray(nums);
+
+TestMyArray.Show();
+TestMyArray.Show("it's my array!!!");
+
+//Task 2
+
+Console.WriteLine();
+Console.WriteLine("Max value: " + TestMyArray.Max());
+Console.WriteLine("Min value: " + TestMyArray.Min());
+Console.WriteLine("Average value: " + TestMyArray.Avg());
+
+Console.WriteLine("Is there a number 4 in array: " + TestMyArray.Search(4));
+Console.WriteLine("Is there a number 1 in array: " + TestMyArray.Search(1));
+Console.WriteLine("Is there a number 6 in array: " + TestMyArray.Search(6));
+Console.WriteLine();
+
+//Task 3
+
+TestMyArray.Show("before sorting: ");
+Console.WriteLine();
+TestMyArray.SortAsc();
+TestMyArray.Show("after SortAsc: ");
+Console.WriteLine();
+
+TestMyArray.SortDesc();
+TestMyArray.Show("after SortDesc:");
+Console.WriteLine();
+
+TestMyArray.SortByParam(true);
+TestMyArray.Show("after SortByParam(true):");
+Console.WriteLine();
+
+TestMyArray.SortByParam(false);
+TestMyArray.Show("after SortByParam(false):");
+Console.WriteLine();
